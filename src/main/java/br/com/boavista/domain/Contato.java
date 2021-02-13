4c86858d3929eadd9349b2908b4aa87d58194205
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-02-13 17:53:46 -0200

    Criada e mapeada classe contato

---
+++ src/main/java/br/com/boavista/domain/Contato.java
@@ -0,0 +1,33 @@
+package br.com.boavista.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+
+@Embeddable
+@Data
+public class Contato implements Serializable {
+
+	private static final long serialVersionUID = -6244387740739026823L;
+
+	@Size(max = 14)
+	@Column(length = 14)
+	private String telefone;
+
+	@Size(max = 15)
+	@Column(length = 15)
+	private String celular;
+
+	@Size(max = 15)
+	@Column(length = 15)
+	private String whatsapp;
+
+	@Email
+	private String email;
+
+}
