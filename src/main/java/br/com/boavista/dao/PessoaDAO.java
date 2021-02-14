9ed30e55bf199a1a2951ecc3920442e1cdebf393
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-02-14 17:01:57 -0200

    Criado metodo existe nome

--- src/main/java/br/com/boavista/dao/PessoaDAO.java
+++ src/main/java/br/com/boavista/dao/PessoaDAO.java
@@ -1,7 +1,25 @@
 package br.com.boavista.dao;
 
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+
 import br.com.boavista.domain.Pessoa;
+import br.com.boavista.util.HibernateUtil;
 
 public class PessoaDAO extends GenericDAO<Pessoa>{
+	
+	@SuppressWarnings("unchecked")
+	public boolean existeNome(String nome) throws RuntimeException {
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
+		List<Pessoa> pessoas = null;
+
+		Query consulta = sessao.getNamedQuery("Pessoa.existeNome");
+		consulta.setString(0, nome);
+		pessoas = consulta.list();
+
+		return pessoas.size() > 0;
+	}
 
 }
