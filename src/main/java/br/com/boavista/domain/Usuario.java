5b7a569fd74637dcbdbd721f9edad74d823e9444
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-02-15 00:34:35 -0200

    Criada entidade Usuario

---
+++ src/main/java/br/com/boavista/domain/Usuario.java
@@ -0,0 +1,43 @@
+package br.com.boavista.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Table(name = "usuario")
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NamedQueries({
+	@NamedQuery(name = "Usuario.existeNome", query = "SELECT usuario FROM Usuario usuario WHERE usuario.login = ?") })
+public class Usuario extends AbstractEntity {
+
+	private static final long serialVersionUID = 717443138100890749L;
+
+	@NotEmpty(message = "O preenchimento do campo nome é obrigatório.")
+	@Size(min = 5, max = 50, message = "Tamanho inválido para o campo nome (5 - 50)")
+	@Column(name = "nome", nullable = false)
+	private String nome;
+
+	@NotEmpty(message = "O preenchimento do campo login é obrigatório.")
+	@Size(min = 5, max = 50, message = "Tamanho inválido para o campo login (5 - 50)")
+	@Column(name = "login", nullable = false)
+	private String login;
+
+	@NotEmpty(message = "O preenchimento do campo senha é obrigatório.")
+	@Size(min = 5, max = 10, message = "Tamanho inválido para o campo senha (5 - 10)")
+	@Column(name = "senha", nullable = false)
+	private String senha;
+
+	public void setNome(String nome) {
+		this.nome = nome.toUpperCase();
+	}
+
+}
